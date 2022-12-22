eb0b31adbc2737474a77e05ae5f3a672f94ce5b8
AnnJakubel <annabel.jakubel@gmail.com>
2022-12-22 20:53:25 +0200

    Solved errors for liquibase

--- src/main/java/ee/annjakubel/petmanager/controller/PetController.java
+++ src/main/java/ee/annjakubel/petmanager/controller/PetController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,4 +28,6 @@ public class PetController {
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(petRepository.findAll());
     }
+
+    @PutMapping
 }
