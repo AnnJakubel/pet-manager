0743a6c0742a7f4c7fa9d4f29ec3328f1f68d9e0
AnnJakubel <annabel.jakubel@gmail.com>
2022-12-22 21:37:26 +0200

    Pet .java @NotNull

--- src/main/java/ee/annjakubel/petmanager/model/Pet.java
+++ src/main/java/ee/annjakubel/petmanager/model/Pet.java
@@ -1,6 +1,7 @@
 package ee.annjakubel.petmanager.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,7 +17,13 @@ public class Pet {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull(message = "field must not be null")
     private String animalType;
+
+    @NotNull(message = "field must not be null")
     private String furColor;
+
+    @NotNull(message = "field must not be null")
     private String cOfOrigin;
 }
