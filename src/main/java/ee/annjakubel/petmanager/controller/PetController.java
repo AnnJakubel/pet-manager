b1e441f4b16da846415933ff6e4e59128de6f275
AnnJakubel <annabel.jakubel@gmail.com>
2022-12-22 21:26:14 +0200

    @Putmapping, PetService (updatePet)

--- src/main/java/ee/annjakubel/petmanager/controller/PetController.java
+++ src/main/java/ee/annjakubel/petmanager/controller/PetController.java
@@ -6,10 +6,7 @@ import ee.annjakubel.petmanager.service.PetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,5 +26,11 @@ public class PetController {
                 .body(petRepository.findAll());
     }
 
-    @PutMapping
+    @PutMapping("pets")
+    public ResponseEntity<Pet> editPet(@RequestBody Pet pet) {
+        petService.updatePet(pet);
+        petRepository.save(pet);
+        return ResponseEntity.ok()
+                .body(petRepository.findById(pet.getId()).get());
+    }
 }
--- src/main/java/ee/annjakubel/petmanager/service/PetService.java
+++ src/main/java/ee/annjakubel/petmanager/service/PetService.java
@@ -1,7 +1,26 @@
 package ee.annjakubel.petmanager.service;
 
+import ee.annjakubel.petmanager.model.Pet;
+import ee.annjakubel.petmanager.repository.PetRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PetService {
+
+    @Autowired
+    PetRepository petRepository;
+    public Pet updatePet(Pet pet) {
+        String newAnimalType = pet.getAnimalType();
+        String newFurColor = pet.getFurColor();
+        String newCOfOrigin = pet.getCOfOrigin();
+
+        Pet updatedPet = petRepository.findById(pet.getId()).get();
+        updatedPet.setAnimalType(newAnimalType);
+        updatedPet.setAnimalType(newAnimalType);
+        updatedPet.setFurColor(newFurColor);
+        updatedPet.setCOfOrigin(newCOfOrigin);
+
+        return updatedPet;
+    }
 }
