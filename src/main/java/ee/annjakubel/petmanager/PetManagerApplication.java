2e1c4e13cb571d23a9d0b539b20156bafd063e76
AnnJakubel <annabel.jakubel@gmail.com>
2022-12-29 20:26:57 +0200

    Documentation, Spring Security

--- src/main/java/ee/annjakubel/petmanager/PetManagerApplication.java
+++ src/main/java/ee/annjakubel/petmanager/PetManagerApplication.java
@@ -3,8 +3,10 @@ package ee.annjakubel.petmanager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity
 public class PetManagerApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/ee/annjakubel/petmanager/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package ee.annjakubel.petmanager.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        return http
+                .csrf().disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/").permitAll()
+                .requestMatchers("/home").hasRole("USER")
+                .requestMatchers("/add-pet").hasRole("USER")
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic()
+                .and().build();
+    }
+}
